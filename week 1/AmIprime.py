41df50154cf1e29e7863c783fbb05090d2532a3e
Umair-Manzoor-47 <76640178+Umair-Manzoor-47@users.noreply.github.com>
2021-03-02 08:06:45 -0800

    Update AmIprime.py
    just trying to check if I can edit repositries

--- week 1/AmIprime.py
+++ week 1/AmIprime.py
@@ -3,8 +3,8 @@ number = int(input("enter number : "))
 counter = 0
 
 # checking if divided more than 1 
-for i in range(1, number) :
-    if number % i == 0 :
+for j in range(1, number) :
+    if number % j == 0 :
         counter += 1
 
 #printing if number id divided by more than 2 terms 
